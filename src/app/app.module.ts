6cf4b9802689eb1dbb220eceb4ec2a9c62f12f63
赵经晖 <zhaojinghui@swifthealth.cn>
2020-04-08 12:22:31 +0800

    PackageAdd: ng-zorro-antd, tongchang-lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router'
+import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TongchangLibModule } from 'tongchang-lib'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import zh from '@angular/common/locales/zh';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -14,8 +20,16 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgZorroAntdModule,
+    BrowserAnimationsModule,
+    TongchangLibModule.forRoot({
+      apiBase: '/',
+      fileApi: '/api/file',
+      headers: {},
+      authRequireCode: -9,
+    })
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

9b5412dee9bc7ea87c2ee6d7ae3d9d958c16c529
tianyouyou <tianyouyou@swifthealth.cn>
2021-01-08 15:41:50 +0800

    温湿度报警处理页面接口联调；

--- src/app/manage/monit/alarm-handl/alarm-handl/alarm-handl.component.ts
+++ src/app/manage/monit/alarm-handl/alarm-handl/alarm-handl.component.ts
@@ -29,8 +29,8 @@ export class AlarmHandlComponent implements OnInit {
   gridActions: GridAction[] = [];
   condition = {
     cId: '',
-    start: null,
-    end: null,
+    start: new Date(),
+    end: new Date(),
     type: '温度',
     status: 'false',
   };
@@ -170,7 +170,7 @@ export class AlarmHandlComponent implements OnInit {
       .subscribe((res) => {
         this.loading = false;
         if (res.code === 0) {
-          this.listOfData = res.data.list;
+          // this.listOfData = res.data.list;
         }
       });
   }
--- src/app/manage/monit/alarm-handl/handle-state-form/handle-state-form.component.ts
+++ src/app/manage/monit/alarm-handl/handle-state-form/handle-state-form.component.ts
@@ -1,38 +1,33 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-// import { HandleState } from "@/model/HouseMonit";
-import { TableList } from "../alarm-handl/alarm-handl.component";
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: "app-handle-state-form",
-  templateUrl: "./handle-state-form.component.html",
-  styleUrls: ["./handle-state-form.component.scss"],
+  selector: 'app-handle-state-form',
+  templateUrl: './handle-state-form.component.html',
+  styleUrls: ['./handle-state-form.component.scss'],
 })
 export class HandleStateFormComponent implements OnInit {
-  @Input() house: TableList;
+  @Input() house: {};
+  form: FormGroup;
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.formInit(this.house);
   }
 
-  form: FormGroup;
-
-  //弹框中表单的初始化
-  formInit(house: TableList) {
+  submitForm(): void {
+    for (const i in this.form.controls) {
+      this.form.controls[i].markAsDirty();
+      this.form.controls[i].updateValueAndValidity();
+    }
+  }
+  // 弹框中表单的初始化
+  formInit(house) {
     this.form = this.fb.group({
       cause: [house.cause, [Validators.required]],
       method: [house.method, [Validators.required]],
       remarks: [house.remarks],
       processor: [house.processor, [Validators.required]],
     });
-  }
-
-  //获取表单的值
-  get formVal() {
-    return {
-      ...this.house,
-      ...this.form.getRawValue(),
-    };
   }
 }

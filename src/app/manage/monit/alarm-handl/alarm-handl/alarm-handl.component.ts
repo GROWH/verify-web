d85188d97ad6c692ff4a5105c77b19866c5d8ada
zhaohuan <zhaohuan@swifthealth.cn>
2021-03-04 10:28:52 +0800

    湿温度报警处理传参修改

--- src/app/manage/monit/alarm-handl/alarm-handl/alarm-handl.component.ts
+++ src/app/manage/monit/alarm-handl/alarm-handl/alarm-handl.component.ts
@@ -152,10 +152,12 @@ export class AlarmHandlComponent implements OnInit {
 
   // 初始出请求
   getData() {
+    
     const params = this.condition;
     const start = '\'' + format(params.start, 'YYYY-MM-DD HH:mm:ss.SSS') + '\'';
     const end = '\'' + format(params.end, 'YYYY-MM-DD HH:mm:ss.SSS') + '\'';
-    const type = '\'' + params.type + '\'';
+    console.log(params.type);
+    const type =params.type==null? params.type : '\''+ (params.type) +'\'' ;
     const searchParam =
       '&pid=' +
       params.cId +
@@ -176,6 +178,7 @@ export class AlarmHandlComponent implements OnInit {
         this.loading = false;
         if (res.code === 0) {
           this.listOfData = res.data.list;
+          console.log(this.listOfData);
         }
       });
   }
@@ -224,6 +227,6 @@ export class TableList {
   method: string; // 处理方法
   remarks: string; // 处理人
   processor: string; // 备注
-  version: string;
+  version: string;  //版本
   id: any;
 }
